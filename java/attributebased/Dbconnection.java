c96920bb7b32f61e76ad35c8ee66e02263bafb33
Yashaswini Kondakindi <109444074+yashaswinikbr@users.noreply.github.com>
2024-05-31 10:28:37 -0400

    Update Dbconnection.java

--- java/attributebased/Dbconnection.java
+++ java/attributebased/Dbconnection.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package attributebased;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- *
- * @author java2
- */
+
 public class Dbconnection {
 
     public static Connection getConnection() {
